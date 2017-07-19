07a28ebd781b8d762967f9bacecd428ab88b12fd
Bas Jansen <bas.jansen@infoaction.nl>
2017-07-19 10:51:18 +0200

    Synchronized consistency check

--- Views/View_ConsistencyCheck.sql
+++ Views/View_ConsistencyCheck.sql
@@ -1,27 +1,65 @@
-SELECT DISTINCT a.PlanVersion,
-                a.SKU + ' - ' + a.Period AS MissingObject,
-                                'No FAP for SKU in period' AS ErrorType
-FROM tblFacts AS a
-LEFT JOIN tblFAP AS b ON a.planversion = b.planversion
-AND a.SKU = b.SKU
-AND a.Period = b.Period
-WHERE b.FAPBox IS NULL
-  AND a.Forecast = 'yes'
-UNION ALL
-SELECT DISTINCT a.PlanVersion,
-                a.SKU,
-                'SKU not in SKU table'
-FROM tblFacts AS a
-LEFT JOIN tblSKU AS b ON a.SKU = b.SKU
-WHERE b.SKU IS NULL
-UNION ALL
-SELECT DISTINCT a.PlanVersion,
-                a.SKU,
-                'Missing Weight or Packs per Box'
-FROM tblFacts AS a
-LEFT JOIN tblSKU AS b ON a.SKU = b.SKU
-WHERE b.SKU IS NOT NULL
-  AND (b.WeightInKg = 0
-       OR b.PackPerBox = 0
-       OR ISNULL(b.WeightInKg)
-       OR ISNULL(b.PackPerBox))
+SELECT DISTINCT a.planversion, 
+                a.sku + ' - ' + a.period   AS MissingObject, 
+                'No FAP for SKU in period' AS ErrorType 
+FROM   tblfacts AS a 
+       LEFT JOIN tblfap AS b 
+              ON a.planversion = b.planversion 
+                 AND a.sku = b.sku 
+                 AND a.period = b.period 
+WHERE  b.fapbox IS NULL 
+       AND a.forecast = 'yes' 
+UNION ALL 
+SELECT DISTINCT a.planversion, 
+                a.sku, 
+                'SKU not in SKU table' 
+FROM   tblfacts AS a 
+       LEFT JOIN tblsku AS b 
+              ON a.sku = b.sku 
+WHERE  b.sku IS NULL 
+UNION ALL 
+SELECT DISTINCT a.planversion, 
+                a.sku, 
+                'Missing Weight or Packs per Box' 
+FROM   tblfacts AS a 
+       LEFT JOIN tblsku AS b 
+              ON a.sku = b.sku 
+WHERE  b.sku IS NOT NULL 
+       AND ( b.weightinkg = 0 
+              OR b.packperbox = 0 
+              OR Isnull(b.weightinkg) 
+              OR Isnull(b.packperbox) ) 
+UNION ALL 
+SELECT DISTINCT a.planversion, 
+                a.sku + ' - ' + a.customer + '-' + a.period, 
+                'No NIS for SKU and Customer in period' 
+FROM   tblfacts AS a 
+       LEFT JOIN tblnis AS b 
+              ON a.planversion = b.planversion 
+                 AND a.sku = b.sku 
+                 AND a.customer = b.customer 
+                 AND a.period = b.period 
+WHERE  a.forecast = 'yes' 
+       AND b.nisbox IS NULL 
+GROUP  BY a.planversion, 
+          a.sku, 
+          a.customer, 
+          a.period, 
+          b.sku, 
+          b.nisbox 
+HAVING Sum(a.volume) <> 0 
+UNION ALL 
+SELECT DISTINCT a.planversion, 
+                a.sku + ' - ' + a.period, 
+                'No COGS for SKU in period' 
+FROM   tblfacts AS a 
+       LEFT JOIN tblcostinput AS b 
+              ON a.planversion = b.planversion 
+                 AND a.sku = b.sku 
+                 AND a.period = b.period 
+WHERE  a.forecast = 'yes' 
+       AND b.sku IS NULL 
+GROUP  BY a.planversion, 
+          a.sku, 
+          a.period, 
+          b.sku 
+HAVING Sum(a.volume) <> 0;   
